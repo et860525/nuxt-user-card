e5edd2947948c2cedfaa505bd00c67117900b07c
Mango <et860525@gmail.com>
2024-04-12 22:18:47 +0800

    Init project
      - Nuxt.js
      - Nuxt UI

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useHead({
+  title: "User Card",
+  meta: [{ name: "description", content: "My amazing site." }],
+});
+</script>
+
+<template>
+  <div class="p-5 max-w-3xl mx-auto">
+    <h1 class="text-2xl font-bold">User Card</h1>
+  </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useHead({
+  title: "Login",
+  meta: [{ name: "description", content: "Login page." }],
+});
+</script>
+
+<template>
+  <div class="p-5">
+    <h1 class="text-2xl font-bold">Login Page</h1>
+  </div>
+</template>
