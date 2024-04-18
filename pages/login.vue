7091c5238e9fa22eca30dbabaa1c678724aa2f88
Mango <et860525@gmail.com>
2024-04-18 17:19:51 +0800

    Login page form (without real function)

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
+import { z } from "zod";
+import type { FormSubmitEvent } from "#ui/types";
 useHead({
   title: "Login",
   meta: [{ name: "description", content: "Login page." }],
 });
+
+const schema = z.object({
+  email: z.string().email("Invalid email"),
+  password: z.string().min(8, "Must be at least 8 characters"),
+});
+
+type Schema = z.output<typeof schema>;
+
+const state = reactive({
+  email: undefined,
+  password: undefined,
+});
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  // Do something with data
+  console.log(event.data.email);
+}
 </script>
 
 <template>
@@ -12,5 +31,16 @@ useHead({
       <h1 class="text-2xl font-bold text-center">Login Page</h1>
       <DarkModeToggle />
     </div>
+    <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+      <UFormGroup label="Email" name="email">
+        <UInput v-model="state.email" />
+      </UFormGroup>
+
+      <UFormGroup label="Password" name="password">
+        <UInput v-model="state.password" type="password" />
+      </UFormGroup>
+
+      <UButton type="submit"> Submit </UButton>
+    </UForm>
   </div>
 </template>
